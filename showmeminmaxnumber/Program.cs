d71bc1a9da9a400cc1aee82e220b4ee641a68f6e
hwon22 <s2018s06@e-mirim.hs.kr>
2020-05-12 22:01:21 +0900

    최댓값, 최솟값 출력 실습

--- showmeminmaxnumber/Program.cs
+++ showmeminmaxnumber/Program.cs
@@ -10,6 +10,31 @@ namespace showmeminmaxnumber
     {
         static void Main(string[] args)
         {
+            string input;
+            int max = 0;
+            int min = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("숫자를 입력해주세요: ");
+                input = Console.ReadLine();
+                if (i == 0){
+                     min = int.Parse(input);
+                     max = int.Parse(input);
+                }
+                else
+                {
+                    if (int.Parse(input) > max){
+                         max = int.Parse(input);
+                    }
+                    if (int.Parse(input) < min)
+                    {
+                        min = int.Parse(input);
+                    }
+                }
+            }
+                Console.WriteLine("------------------------");
+                Console.WriteLine("가장 큰 수: " + max);
+                Console.WriteLine("가장 작은 수: " + min);
         }
     }
 }
